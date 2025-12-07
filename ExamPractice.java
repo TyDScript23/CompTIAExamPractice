cfe2445ce9884e1616e935ddddb941e0fe00f392
Tyler Davis <112653240+TyDScript23@users.noreply.github.com>
2025-12-07 12:52:15 -0500

    Finished the program!

--- ExamPractice.java
+++ ExamPractice.java
@@ -25,12 +25,12 @@ public class ExamPractice {
         }
 
         //now let's print out the contents of each drive
-        System.out.println("Drive 1 contents:");
+        System.out.println("RAID 0 Drive 1 contents:");
         for(byte b : drive1){
             System.out.print(b + " ");
         }
 
-        System.out.println("\nDrive 2 contents:");
+        System.out.println("\nRAID 0 Drive 2 contents:");
         for(byte b : drive2){
             System.out.print(b + " ");
         }
@@ -50,14 +50,15 @@ public class ExamPractice {
         }
 
         //now let's print out the contents of each mirrored drive
+        System.out.println();
         System.out.println(); 
-        System.out.println("Mirror Drive 1 contents:");
+        System.out.println("RAID 1 Drive 1 contents:");
 
         for(byte b : mirrorDrive1){
             System.out.print(b + " ");
         }
 
-        System.out.println("\nMirror Drive 2 contents:");
+        System.out.println("\nRAID 1 Drive 2 contents:");
 
         for(byte b : mirrorDrive2){
             System.out.print(b + " ");
@@ -67,10 +68,273 @@ public class ExamPractice {
         //RAID 5 - STRIPING WITH PARITY EXAMPLE
         //##############################################################
 
-        //let's make four undefined array of bytes to represent different hard drive memories
-        byte[] raid5Drive1 = new byte[6];
-        byte[] raid5Drive2 = new byte[6];
-        byte[] raid5Drive3 = new byte[6]; //this will hold parity information
-        byte[] raid5Drive4 = new byte[6]; //not used in this example
+        //make a new byte array here for better representation
+        byte[] raid5Data = new byte[12];
+
+        //then make four drives to represent the hard drives
+        byte[] raid5Drive1 = new byte[4];
+        byte[] raid5Drive2 = new byte[4];
+        byte[] raid5Drive3 = new byte[4];
+        byte[] raid5Drive4 = new byte[4];
+
+        //fill the data array with some values
+        for(int i = 0; i < raid5Data.length; i++){
+            raid5Data[i] = (byte)(i); //filling the data array with some values
+        }
+
+        int dataCounter = 0;
+        int parityCatcher = 1; //determine which drive gets the parity byte
+
+        //NOTE: In a real RAID 5 implementation, the parity byte would be calculated based on the data bytes. 
+        // The parity byte is represented as -1 for simplicity.
+
+        //now let's use striping with parity to feed the data into the four drives
+        //we add four to accomadte the extra parity bytes equal to the number of drives available
+        for(int i = 0; i < (raid5Data.length + 4); i++){
+            
+            int driveIndex = i % 4; //determine which drive to place data
+            int dataIndex = i / 4;//determine the index within the drive to place data
+
+
+            switch(driveIndex){
+                case 0:
+                    System.out.println("ParityCatcher: " + parityCatcher);
+                    if(parityCatcher == 4) {
+                        System.out.println("Placing parity on drive 1 at index " + dataIndex);
+                        //place the parity at the index
+                        raid5Drive1[dataIndex] = (byte)(-1);
+                    }
+                    else {
+                        System.out.println("DataCounter: " + dataCounter);
+                        raid5Drive1[dataIndex] = raid5Data[dataCounter];
+                        dataCounter++;
+                    }
+                    break;
+                case 1:
+                    System.out.println("ParityCatcher: " + parityCatcher);
+                    if(parityCatcher == 3) {
+                        System.out.println("Placing parity on drive 2 at index " + dataIndex);
+                        //place the parity at the index
+                        raid5Drive2[dataIndex] = (byte)(-1);
+                    }
+                    else {
+                        System.out.println("DataCounter: " + dataCounter);
+                        raid5Drive2[dataIndex] = raid5Data[dataCounter];
+                        dataCounter++;
+                    }
+                    break;
+                case 2:
+                    System.out.println("ParityCatcher: " + parityCatcher);
+                    if(parityCatcher == 2) {
+                        System.out.println("Placing parity on drive 3 at index " + dataIndex);
+                        //place the parity at the index
+                        raid5Drive3[dataIndex] = (byte)(-1);
+                    }
+                    else {
+                        System.out.println("DataCounter: " + dataCounter);
+                        raid5Drive3[dataIndex] = raid5Data[dataCounter];
+                        dataCounter++;
+                    }
+                    break;
+                case 3:
+                    System.out.println("ParityCatcher: " + parityCatcher);
+                    if(parityCatcher == 1) {
+                        System.out.println("Placing parity at drive 4, index " + dataIndex);
+                        //place the parity at the index
+                        raid5Drive4[dataIndex] = (byte)(-1);
+                    }
+                    else {
+                        System.out.println("DataCounter: " + dataCounter);
+                        raid5Drive4[dataIndex] = raid5Data[dataCounter];
+                        dataCounter++;
+                    }
+                    parityCatcher++;
+                    break;
+            }
+        }
+
+        //now let's print out the contents of each RAID 5 drive
+        System.out.println();
+        System.out.println();
+        System.out.println("RAID 5 Drive 1 contents:");
+        for(byte b : raid5Drive1){
+            System.out.print(b + " ");
+        }
+        System.out.println("\nRAID 5 Drive 2 contents:");
+        for(byte b : raid5Drive2){
+            System.out.print(b + " ");
+        }
+        System.out.println("\nRAID 5 Drive 3 contents:");
+        for(byte b : raid5Drive3){
+            System.out.print(b + " ");
+        }
+        System.out.println("\nRAID 5 Drive 4 contents:");
+        for(byte b : raid5Drive4){
+            System.out.print(b + " ");
+        }
+
+        //##############################################################
+        //RAID 6 - STRIPING WITH 2X PARITY EXAMPLE
+        //##############################################################
+
+        //here are the 5 hard drives we will be using
+        byte[] raid6Drive1 = new byte[4];
+        byte[] raid6Drive2 = new byte[4];
+        byte[] raid6Drive3 = new byte[4];
+        byte[] raid6Drive4 = new byte[4];
+        byte[] raid6Drive5 = new byte[4];
+
+        //NOTE: In a real RAID 6 implementation, the parity bytes would be calculated based on the data bytes.
+        // The parity bytes are represented as -1 and -2 for simplicity.
+
+        int dataCounter6 = 0;
+        int parityCatcher6 = 1; //determine which drive gets the parity byte
+
+        //now let's use striping with double parity to feed the data into the five drives
+        for(int i = 0; i < (raid5Data.length + 8); i++){
+            
+            int driveIndex = i % 5; //determine which drive to place data
+            int dataIndex = i / 5;//determine the index within the drive to place data
+
+            switch(driveIndex){
+                case 0:
+                    if(parityCatcher6 == 4) {
+                        //place the parity at the index
+                        raid6Drive1[dataIndex] = (byte)(-1) ;
+                    }
+                    else {
+                        raid6Drive1[dataIndex] = raid5Data[dataCounter6];
+                        dataCounter6++;
+                    }
+                    break;
+                case 1:
+                    if(parityCatcher6 == 3 || parityCatcher6 == 4) {
+                        //place the parity at the index
+                        raid6Drive2[dataIndex] = (parityCatcher6 == 3) ? (byte)(-1) : (byte)(-2);
+                    }
+                    else {
+                        raid6Drive2[dataIndex] = raid5Data[dataCounter6];
+                        dataCounter6++;
+                    }
+                    break;
+                case 2:
+                    if(parityCatcher6 == 2 || parityCatcher6 == 3) {
+                        //place the parity at the index
+                        raid6Drive3[dataIndex] = (parityCatcher6 == 2) ? (byte)(-1) : (byte)(-2);
+                    }
+                    else {
+                        raid6Drive3[dataIndex] = raid5Data[dataCounter6];
+                        dataCounter6++;
+                    }
+                    break;
+                case 3:
+                    if(parityCatcher6 == 1 || parityCatcher6 == 2) {
+                        //place the parity at the index
+                        raid6Drive4[dataIndex] = (parityCatcher6 == 1) ? (byte)(-1) : (byte)(-2);
+                    }
+                    else {
+                        raid6Drive4[dataIndex] = raid5Data[dataCounter6];
+                        dataCounter6++;
+                    }
+                    break;
+                case 4:
+                    if(parityCatcher6 == 1 ) {
+                        //place the parity at the index
+                        raid6Drive5[dataIndex] = (byte)(-2);
+                    }
+                    else {
+                        raid6Drive5[dataIndex] = raid5Data[dataCounter6];
+                        dataCounter6++;
+                    }
+                    parityCatcher6++;
+            }
+        }
+
+        //now let's print out the contents of each RAID 6 drive
+        System.out.println();
+        System.out.println();
+        System.out.println("RAID 6 Drive 1 contents:");
+        for(byte b : raid6Drive1){
+            System.out.print(b + " ");
+        }   
+        System.out.println("\nRAID 6 Drive 2 contents:");
+        for(byte b : raid6Drive2){
+            System.out.print(b + " ");
+        }
+        System.out.println("\nRAID 6 Drive 3 contents:");
+        for(byte b : raid6Drive3){
+            System.out.print(b + " ");
+        }
+        System.out.println("\nRAID 6 Drive 4 contents:");
+        for(byte b : raid6Drive4){
+            System.out.print(b + " ");
+        }
+        System.out.println("\nRAID 6 Drive 5 contents:");
+        for(byte b : raid6Drive5){
+            System.out.print(b + " ");
+        }
+
+        //##############################################################
+        //RAID 10 - STRIPING WITH MIRRORING EXAMPLE
+        //##############################################################
+
+        //create the data to be split amongst the drives
+
+        //make four drives to represent the hard drives
+        byte[] raid10Drive1 = new byte[4];
+        byte[] raid10Drive2 = new byte[4];
+        byte[] raid10Drive3 = new byte[4];
+        byte[] raid10Drive4 = new byte[4];
+        byte[] raid10Drive5 = new byte[4];
+        byte[] raid10Drive6 = new byte[4];
+
+        //now let's use striping with mirroring to feed the data into the four drives
+        for(int i = 0; i < raid5Data.length; i++){
+            int driveIndex = i % 3;
+            int dataIndex = i / 3;
+
+            switch(driveIndex){
+                case 0:
+                    raid10Drive1[dataIndex] = raid5Data[i];
+                    raid10Drive2[dataIndex] = raid5Data[i]; //mirror
+                    break;
+                case 1:
+                    raid10Drive3[dataIndex] = raid5Data[i];
+                    raid10Drive4[dataIndex] = raid5Data[i]; //mirror
+                    break;
+                case 2:
+                    raid10Drive5[dataIndex] = raid5Data[i];
+                    raid10Drive6[dataIndex] = raid5Data[i]; //mirror
+                    break;
+            }
+        }
+
+        //now let's print out the contents of each RAID 10 drive
+        System.out.println();
+        System.out.println();
+        System.out.println("RAID 10 Drive 1 contents:");
+        for(byte b : raid10Drive1){
+            System.out.print(b + " ");
+        }
+        System.out.println("\nRAID 10 Drive 2 contents:");
+        for(byte b : raid10Drive2){
+            System.out.print(b + " ");
+        }
+        System.out.println("\nRAID 10 Drive 3 contents:");
+        for(byte b : raid10Drive3){
+            System.out.print(b + " ");
+        }
+        System.out.println("\nRAID 10 Drive 4 contents:");
+        for(byte b : raid10Drive4){
+            System.out.print(b + " ");
+        }
+        System.out.println("\nRAID 10 Drive 5 contents:");
+        for(byte b : raid10Drive5){
+            System.out.print(b + " ");
+        }
+        System.out.println("\nRAID 10 Drive 6 contents:");
+        for(byte b : raid10Drive6){
+            System.out.print(b + " ");
+        }
     }
 }
